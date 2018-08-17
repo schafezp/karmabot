9a5c0e51f7ca6c20eb224de27e8b31421e69c675
Zachary Schafer <schafezp@rose-hulman.edu>
2018-08-17 16:51:12 -0400

    seperates functions and main body

--- bot.py
+++ bot.py
@@ -11,52 +11,67 @@ dispatcher = updater.dispatcher
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
 
 def echo(bot, update):
+    print(dir(bot))
+    print(dir(update))
+    print(bot.getMe())
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-
 
 def caps(bot, update, args):
      text_caps = ' '.join(args).upper()
      bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
-caps_handler = CommandHandler('caps', caps, pass_args=True)
-dispatcher.add_handler(caps_handler)
-
 def pluskarma(bot, update, args):
-     bot.send_message(chat_id=update.message.chat_id, text="you threw +1 karma into the void")
-
-plus_karma_handler = CommandHandler('+1', pluskarma, pass_args=True)
-dispatcher.add_handler(plus_karma_handler)
+    bot.send_message(chat_id=update.message.chat_id, text="you threw +1 karma into the void")
 
 def minuskarma(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text="you threw -1 karma into the void")
-
-minus_karma_handler = CommandHandler('-1', minuskarma, pass_args=True)
-dispatcher.add_handler(minus_karma_handler)
-
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
-
-dispatcher.add_error_handler(error)
-
+""" this command runs last and lets the user know their command was not understood """
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
 
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
 
-updater.start_polling()
-updater.idle()
+def main():
+    """Start the bot """
+
+    from telegram.ext import CommandHandler
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    from telegram.ext import MessageHandler, Filters
+    echo_handler = MessageHandler(Filters.text, echo)
+    dispatcher.add_handler(echo_handler)
+
+    caps_handler = CommandHandler('caps', caps, pass_args=True)
+    dispatcher.add_handler(caps_handler)
+
+    plus_karma_handler = CommandHandler('+1', pluskarma, pass_args=True)
+    dispatcher.add_handler(plus_karma_handler)
+
+    plus_karma_handler = CommandHandler('plus1', pluskarma, pass_args=True)
+    dispatcher.add_handler(plus_karma_handler)
+
+    minus_karma_handler = CommandHandler('-1', minuskarma, pass_args=True)
+    dispatcher.add_handler(minus_karma_handler)
+
+    minus_karma_handler = CommandHandler('minus1', minuskarma, pass_args=True)
+    dispatcher.add_handler(minus_karma_handler)
+
+    dispatcher.add_error_handler(error)
 
 
+
+    unknown_handler = MessageHandler(Filters.command, unknown)
+    dispatcher.add_handler(unknown_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
