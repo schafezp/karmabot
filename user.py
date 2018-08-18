6a17f9fece226b317bd1081e62735f730c3a41be
Zachary Schafer <schafezp@rose-hulman.edu>
2018-08-18 03:11:50 -0400

    create represntation of User without username

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import telegram as tg
+from typing import Optional
 
 class User(object):
     """
@@ -6,10 +7,10 @@ class User(object):
     """
     def __init__(self, user: tg.User ):
         self.__karma = 0
-        self.id = user.id
-        self.first_name = user.first_name
-        self.last_name = user.last_name
-        self.username = user.username
+        self.id : int = user.id
+        self.first_name :  str = user.first_name
+        self.last_name : Optional[str] = user.last_name
+        self.username : Optional[str] = user.username
     def get_karma(self):
         return self.__karma
     def give_karma(self):
@@ -17,4 +18,7 @@ class User(object):
     def remove_karma(self):
         self.__karma = self.__karma - 1
     def __str__(self):
+        if self.username is not None:
             return "Username: " + self.username + " karma: " + str(self.get_karma())
+        else:
+            return "First Name: " + self.first_name + " karma: " + str(self.get_karma())
