52036d2371164585af9248ce43e99b0d190e3488
Zachary Schafer <schafezp@rose-hulman.edu>
2018-08-26 15:22:28 -0400

    update schema to enforce uniqueness in user_in_chat(user_id,chat_id) constraint using primary key

--- start-schema.sql
+++ start-schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS telegram_user ( 
+CREATE TABLE IF NOT EXISTS telegram_user ( -- use IF NOT EXISTS to not error if they table does exist
     user_id INTEGER PRIMARY KEY,
     username TEXT,
     first_name TEXT,
@@ -11,26 +11,23 @@ CREATE TABLE IF NOT EXISTS telegram_chat (
 );
 
 CREATE TABLE IF NOT EXISTS user_in_chat (
-    id SERIAL PRIMARY KEY, -- TODO: should user_id, chat_id be the primary keys instead?
     user_id INTEGER REFERENCES telegram_user(user_id),
     chat_id INTEGER REFERENCES telegram_chat(chat_id),
-    karma integer
+    karma integer,
+    PRIMARY KEY (user_id,chat_id)
 );
 
 CREATE TABLE IF NOT EXISTS telegram_message (
     message_id INTEGER PRIMARY KEY,
     chat_id INTEGER REFERENCES telegram_chat(chat_id),
-    author_user_in_chat_id INTEGER REFERENCES user_in_chat(id),
-    message_text TEXT
-    
+    author_user_id INTEGER REFERENCES telegram_user(user_id),
+    message_text TEXT  
 );
 
 CREATE TABLE IF NOT EXISTS user_reacted_to_message (
     id SERIAL PRIMARY KEY,
-    --TODO: should I use user_id or user_in_chat_id?
-    --user_id INTEGER REFERENCES user_in_chat(user_id),
-    user_in_chat_id INTEGER REFERENCES user_in_chat(id),
+    user_id INTEGER REFERENCES telegram_user(user_id),
     message_id INTEGER REFERENCES telegram_message(message_id),
     react_score INTEGER, --"1" if +1, "-1" if -1,
-    react_message_id TEXT
+    react_message_id INTEGER
 );
