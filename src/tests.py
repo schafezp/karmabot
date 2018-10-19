3f27d2c13fa6f0371734740e4346f844fc52de8a
Zachary Schafer <schafezp@gmail.com>
2018-10-18 21:35:44 -0400

    add function in tests file to clear db

--- src/tests.py
+++ src/tests.py
@@ -1,11 +1,20 @@
 import unittest
 from utils import attempt_connect
+def clear_db_cmds():
+    tables = ["telegram_user", "telegram_chat", "user_in_chat", "telegram_message", "user_reacted_to_message", "command_used"]
+    cmds = ["truncate " + tablename + " cascade" for tablename in tables]
+    conn = attempt_connect(DATABASE="test_db")
+    with conn:
+        with conn.cursor() as crs:
+            for cmd in clear_db_cmds():
+                print(cmd)
+                crs.execute(cmd,[])
 
 def hello_world():
     return "hello world"
 class MyFirstTests(unittest.TestCase):
     def test_hello(self):
-        conn = attempt_connect(DATABASE="test_db")
+        
         self.assertEqual(hello_world(), 'hello world')
     
 
