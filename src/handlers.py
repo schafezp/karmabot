a0ba569ec6104920844f5743fd144972212f6f6e
Zachary Schafer <schafezp@gmail.com>
2018-10-18 22:01:59 -0400

    fix linter errors

--- src/handlers.py
+++ src/handlers.py
@@ -1,6 +1,11 @@
+"""
+All handlers go here. Putting them in one place will make it easier to mock the services.
+"""
 from models import User, Telegram_Message
 
+
 def user_plus_or_minus_one(author_user: User, reply_to_user: User, chat_id: str, original_message: Telegram_Message, reply_message: Telegram_Message):
+    """When user plus or minus one"""
 
     pass
     
--- src/tests.py
+++ src/tests.py
@@ -1,22 +1,28 @@
+"""
+Module for testing the project.
+"""
 import unittest
 from utils import attempt_connect
 def clear_db_cmds():
+    """ Clears the database"""
     tables = ["telegram_user", "telegram_chat", "user_in_chat", "telegram_message", "user_reacted_to_message", "command_used"]
     cmds = ["truncate " + tablename + " cascade" for tablename in tables]
     conn = attempt_connect(DATABASE="test_db")
     with conn:
         with conn.cursor() as crs:
-            for cmd in clear_db_cmds():
+            for cmd in cmds:
                 print(cmd)
-                crs.execute(cmd,[])
+                crs.execute(cmd, [])
 
 def hello_world():
+    """returns hello world"""
     return "hello world"
 class MyFirstTests(unittest.TestCase):
+    """Test class"""
     def test_hello(self):
-        
+        """tests hello_world()"""
         self.assertEqual(hello_world(), 'hello world')
-    
+
 
 if __name__ == '__main__':
     unittest.main()
