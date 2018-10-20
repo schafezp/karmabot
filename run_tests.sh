9e8f93d1a6c9ec220b598f834f8683a15d4391b0
Zachary Schafer <schafezp@gmail.com>
2018-10-20 16:45:57 -0400

    update run command to use a volume for sessions

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 docker build -f Dockerfile.integration_testing  -t integration_testing .
-docker run --rm integration_testing
+docker run --rm -it -v "${pwd}"/session/:/code/session integration_testing
 
