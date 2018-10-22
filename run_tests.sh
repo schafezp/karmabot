02505a473787ea6fbf6330dcc372d00ff5ede03a
Zachary Schafer <schafezp@gmail.com>
2018-10-21 21:06:36 -0400

    update run tests command  to add env file and volume for session

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 docker build -f Dockerfile.integration_testing  -t integration_testing .
-docker run --rm -it -v "${pwd}"/session/:/code/session integration_testing
+docker run --rm -it -v "${pwd}"/session/:/karmabot/session --env-file .env integration_testing
 
