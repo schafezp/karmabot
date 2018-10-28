a412a9d31f1aacd50d57c9f74ce6695bd956f2c6
Zachary Schafer <schafezp@gmail.com>
2018-10-28 19:08:03 -0400

    remove reply from bot.py

--- karmabot/bot.py
+++ karmabot/bot.py
@@ -63,58 +63,6 @@ def restricted(func):
 #blocks here
 conn = attempt_connect()
 
-
-def reply(bot: tg.Bot, update: tg.Update):
-    """Handler that's run when one user replies to another userself.
-    This handler checks if an upvote or downvote are given"""
-    reply_user = user_from_tg_user(update.message.reply_to_message.from_user)
-    replying_user = user_from_tg_user(update.message.from_user)
-    chat_id = str(update.message.chat_id)
-    chat = Telegram_Chat(chat_id, update.message.chat.title)
-    original_message = Telegram_Message(
-        update.message.reply_to_message.message_id,
-        chat.chat_id,
-        reply_user.id,
-        update.message.reply_to_message.text)
-    reply_message = Telegram_Message(
-        update.message.message_id,
-        chat.chat_id,
-        replying_user.id,
-        update.message.text)
-    reply_text = reply_message.message_text
-
-    if re.match("^([\+pP][1-9][0-9]*|[Pp]{2}).*", reply_text):
-        # if user tried to +1 self themselves
-        # chat id is user_id when the user is talking 1 on 1 with the bot
-        if(replying_user.id == update.message.reply_to_message.from_user.id and chat_id != str(reply_user.id)):
-            default_respose = "USER_FIRST_NAME you cannot +1 yourself"
-            response = pf.get_random_witty_response(conn)
-            if response is None:
-                response = default_respose
-
-            message = response.replace("USER_FIRST_NAME", replying_user.first_name)
-            bot.send_message(chat_id=chat_id, text=message)
-        else:  # user +1 someone else
-            pf.user_reply_to_message(
-                replying_user,
-                reply_user,
-                chat,
-                original_message,
-                reply_message,
-                1,
-                conn)
-            logging.debug("user replying other user")
-            logging.debug(replying_user)
-            logging.debug(reply_user)
-    # user -1 someone else
-    elif re.match("^([\-mM][1-9][0-9]*|[Dd]{2}).*", reply_text):
-        pf.user_reply_to_message(replying_user, reply_user,
-                                 chat, original_message, reply_message, -1, conn)
-        logging.debug("user replying other user")
-        logging.debug(replying_user)
-        logging.debug(reply_user)
-
-
 def start(bot, update):
     """Message sent by bot upon first 1 on 1 interaction with the bot"""
     bot.send_message(
