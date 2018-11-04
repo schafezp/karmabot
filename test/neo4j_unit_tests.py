40ad7354c1d16542d464a68dbb792b5f5f14aac1
Zachary Schafer <zachary.schafer@cigna.com>
2018-11-04 00:21:01 -0400

    add neo4j unit tests

--- test/neo4j_unit_tests.py
+++ test/neo4j_unit_tests.py
@@ -2,12 +2,22 @@ import unittest
 from py2neo import Graph
 
 
+
 class Neo_Test(unittest.TestCase):
     def setUp(self):
         self.graph = Graph(host="neo4j", password="admin")
     def test_connect(self):
-        print(self.graph)
-        print(self.graph.run("MATCH (n:User) return n"))
+        print("show graph")
+        with open("neo4j_unittest_setup.cql") as cql_setup_files:
+            cql_lines = cql_setup_files.readlines()
+            line = " ".join([line.replace("\n","") for line in cql_lines])
+            print("line: " + line)
+            self.graph.run(line)
+            # for line in lines:
+            #     if line != "\n":
+
+
+        print(self.graph.run("MATCH (n) return n").to_table())
 
         # logging.info(self.graph.data("MATCH (n:User) return n"))
 
