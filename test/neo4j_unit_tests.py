fddabd43125a42404e898d8567565011d80188f3
Zachary Schafer <zachary.schafer@cigna.com>
2018-11-04 01:09:16 -0400

    update unit tests to test repo

--- test/neo4j_unit_tests.py
+++ test/neo4j_unit_tests.py
@@ -1,26 +1,22 @@
 import unittest
-from py2neo import Graph
-
-
+from py2neo import Graph, Node
+from karmabot.repo.neo4j_repo import get_all_users
 
 class Neo_Test(unittest.TestCase):
     def setUp(self):
         self.graph = Graph(host="neo4j", password="admin")
-    def test_connect(self):
-        print("show graph")
+        self.graph.run("match (n) detach delete n")
         with open("neo4j_unittest_setup.cql") as cql_setup_files:
             cql_lines = cql_setup_files.readlines()
-            line = " ".join([line.replace("\n","") for line in cql_lines])
-            print("line: " + line)
+            line = " ".join([line.replace("\n", "") for line in cql_lines])
             self.graph.run(line)
-            # for line in lines:
-            #     if line != "\n":
 
-
-        print(self.graph.run("MATCH (n) return n").to_table())
-
-        # logging.info(self.graph.data("MATCH (n:User) return n"))
-
+    def test_get_users(self):
+        data = self.graph.run("MATCH (n:User) return n").data()
+        test_karma_values = [5,3,-1]
+        users = get_all_users(self.graph)
+        karma = [users.karma for user in users]
+        self.assertTrue(test_karma_values == karma)
 
 if __name__ == "__main__":
     unittest.main()
