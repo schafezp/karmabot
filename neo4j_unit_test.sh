4b600673d5b549b61b11ddbec8b20e86e3e49826
Zachary Schafer <zachary.schafer@cigna.com>
2018-11-03 23:49:06 -0400

    add neo4j unit test

--- neo4j_unit_test.sh
+++ neo4j_unit_test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-docker build -f Dockerfile.integration_testing  -t kbot_neo4j_testing . &&
-docker run --rm -it -v "${pwd}"/session/:/karmabot/session --env-file .env integration_testing "python3 -m test.neo4j_unit_tests"
+
+#docker build -f Dockerfile.neo4j_unittest  -t kbot_neo4j_testing . &&
+#docker run --rm -it -v /Users/m80891/personal/karmabot/session/:/karmabot/session --env-file .env kbot_neo4j_testing
+docker-compose -f neo4j_unit_tests.yml up -d --build --no-deps
+docker-compose -f neo4j_unit_tests.yml logs test
--- test/neo4j_unit_tests.py
+++ test/neo4j_unit_tests.py
@@ -1,3 +1,17 @@
-from py2neo import Database
+import unittest
+from py2neo import Graph
 
-db = Database("bolt://camelot.example.com:7687")
+
+class Neo_Test(unittest.TestCase):
+    def setUp(self):
+        self.graph = Graph(host="neo4j", password="admin")
+    def test_connect(self):
+        print(self.graph)
+        print(self.graph.run("MATCH (n:User) return n"))
+
+        # logging.info(self.graph.data("MATCH (n:User) return n"))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
