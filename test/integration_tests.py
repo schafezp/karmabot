c7919ee9a67bcd071ce6abc461a77b4878abfc8f
Zachary Schafer <zachary.schafer@cigna.com>
2018-11-03 21:56:55 -0400

    move integration test setup into common module

--- test/integration_tests.py
+++ test/integration_tests.py
@@ -6,35 +6,19 @@ import re
 from tgintegration import BotIntegrationClient
 from karmabot.responses import START_BOT_RESPONSE, SUCCESSFUL_CLEAR_CHAT, SHOW_KARMA_NO_HISTORY_RESPONSE
 from karmabot.commands_strings import START_COMMAND, CLEAR_CHAT_COMMAND, SHOW_KARMA_COMMAND, USER_INFO_COMMAND, CHAT_INFO_COMMAND, SHOW_KARMA_KEYBOARD_COMMAND
+from .integration_common import setup_bot_client, tear_down_bot_client
+#TODO: move to import module structure invocation from script
 
 
 class IntegrationTests(unittest.TestCase):
     """ Runs intergation tests"""
+    client: BotIntegrationClient
     def setUp(self):
-        """ Sets up the environment"""
-        API_ID = os.environ.get("API_ID")
-        API_HASH = os.environ.get("API_HASH")
-        TEST_BOT_NAME = os.environ.get("TEST_BOT_NAME")
-        if None in [API_HASH, API_ID, TEST_BOT_NAME]:
-            print("API_ID, API_HASH, TEST_BOT_NAME not set")
-            raise ValueError()
+        setup_bot_client(self)
 
-        self.TEST_BOT_NAME = TEST_BOT_NAME
 
-        client = BotIntegrationClient(
-            bot_under_test=TEST_BOT_NAME,
-            session_name='./session/my_account',  # Arbitrary file path to the Pyrogram session file
-            api_id=API_ID,  # See "Requirements" above, ...
-            api_hash=API_HASH,  # alternatively use a `config.ini` file
-            max_wait_response=15,  # Maximum timeout for bot responses
-            min_wait_consecutive=2  # Minimum time to wait for consecutive messages
-        )
-        client.start()
-        #client.clear_chat()
-        self.client = client
     def tearDown(self):
-        self.client.stop()
-        #pass
+        tear_down_bot_client(self)
 
     @unittest.skip("dont care that this works for now")
     def test_start(self):
