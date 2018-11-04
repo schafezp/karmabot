f25db92e511bd284e2b284fbe3436ecfdf2f2b54
Zachary Schafer <zachary.schafer@cigna.com>
2018-11-04 18:22:05 -0500

    make test_get_users pass

--- test/neo4j_unit_tests.py
+++ test/neo4j_unit_tests.py
@@ -2,6 +2,7 @@ import unittest
 from py2neo import Graph, Node
 from karmabot.repo.neo4j_repo import get_all_users
 
+
 class Neo_Test(unittest.TestCase):
     def setUp(self):
         self.graph = Graph(host="neo4j", password="admin")
@@ -12,10 +13,12 @@ class Neo_Test(unittest.TestCase):
             self.graph.run(line)
 
     def test_get_users(self):
-        test_karma_values = [5,3,-1]
+        test_karma_values = [-1, 3, 5]
         users = get_all_users(self.graph)
-        karma = [users.karma for user in users]
-        self.assertTrue(test_karma_values == karma)
+        karma = [user.karma for user in users]
+        karma.sort()
+        self.assertTrue(test_karma_values.sort() == karma.sort())
+
 
 if __name__ == "__main__":
     unittest.main()
