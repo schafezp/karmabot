3f19b62d3ff87f1d5ea3c2efa1d3bf097e525f00
Zachary Schafer <zachary.schafer@cigna.com>
2018-11-04 20:06:21 -0500

    add get users in chat test

--- test/neo4j_unit_tests.py
+++ test/neo4j_unit_tests.py
@@ -1,11 +1,11 @@
 import unittest
 from py2neo import Graph, Node
-from karmabot.repo.neo4j_repo import get_all_users
+from karmabot.repo.neo4j_repo import get_all_users, get_users_in_chat
 
 
 class Neo_Test(unittest.TestCase):
     def setUp(self):
-        self.graph = Graph(host="neo4j", password="admin")
+        self.graph = Graph(host="localhost", password="admin")
         self.graph.run("match (n) detach delete n")
         with open("neo4j_unittest_setup.cql") as cql_setup_files:
             cql_lines = cql_setup_files.readlines()
@@ -18,6 +18,14 @@ class Neo_Test(unittest.TestCase):
         karma = [user.karma for user in users]
         karma.sort()
         self.assertTrue(test_karma_values.sort() == karma.sort())
+
+    def test_get_users_in_chat(self):
+        chat_id = "1"
+        chat_name = "A fun chat"
+        users, chat = get_users_in_chat(self.graph, chat_id)
+        self.assertTrue(chat.chat_id == chat_id)
+        self.assertTrue(chat.chat_name == chat_name)
+
 
 
 if __name__ == "__main__":
