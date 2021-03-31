557f507843bc986c4974755062a074dac0655eec
catboy f <schafezp@gmail.com>
2021-03-30 22:06:43 -0400

    refactor update, context

--- karmabot/annotations.py
+++ karmabot/annotations.py
@@ -5,10 +5,11 @@ def types(func):
     """Used by bot handlers that respond with text.
     ChatAction.Typing is called which makes the bot look like it's typing"""
     @wraps(func)
-    def wrapped(bot, update, *args, **kwargs):
+    def wrapped(update, context,  *args, **kwargs):
         """function to wrap"""
+        bot: tg.Bot = context.bot
         bot.send_chat_action(
             chat_id=update.message.chat_id,
             action=tg.ChatAction.TYPING)
-        return func(bot, update, *args, **kwargs)
+        return func(update, context, *args, **kwargs)
     return wrapped
