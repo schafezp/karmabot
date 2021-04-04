7ee7d8a28209b9a0d895db231a6508a7851e4ef4
Zach <schafezp@gmail.com>
2021-04-04 11:19:26 -0400

    change start response (#77)

--- karmabot/responses.py
+++ karmabot/responses.py
@@ -1,7 +1,7 @@
 """Central place for bot response text strings
 Used to simplify process of changing text responses, and to make tests better
 """
-START_BOT_RESPONSE = "I'm a bot, please talk to me!"
+START_BOT_RESPONSE = "Karmabot has been initiated!"
 SUCCESSFUL_CLEAR_CHAT = "Clearing chat history"
 FAILED_CLEAR_CHAT_DUE_TO_GROUPCHAT = "This is a group chat. Don't delete me!"
 SHOW_KARMA_NO_HISTORY_RESPONSE = "No karma for users in this chat"
